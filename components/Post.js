1fd1b6883c55f602533af7296a13267d663978e9
Manu Anish <manusanish@gmail.com>
2022-05-07 08:29:20 -0400

    More little additions

--- components/Post.js
+++ components/Post.js
@@ -4,7 +4,7 @@ import Button from '@components/Button';
 
 function Post(props){
   return (
-    <div className="p-10 border rounded-xl shadow-md mb-20 pb-0">
+    <div className="p-10 border rounded-xl shadow-md mb-10 pb-0">
       <div className="font-bold text-2xl">{props.title}</div>
       <div className="text-[gray]">Posted at: {props.date}</div>
       <br />
